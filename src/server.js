60a7dbb851e4f5d41376bd295095727d8e89ae1a
Suu01 <92988188+Suu01@users.noreply.github.com>
2023-06-06 22:07:31 +0900

    response

--- src/server.js
+++ src/server.js
@@ -5,6 +5,21 @@ const PORT = 4000;
 
 const app = express();
 
+// const handleHome = (req, res) => console.log("Somebody is trying to go home.");
+const handleHome = (req, res) => {
+  //   console.log(req);
+  //   console.log(res);
+  //   return res.end();
+  return res.send("<h1>I still love you.</h1>");
+};
+
+const handleLogin = (req, res) => {
+  return res.send({ message: "Login here." });
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
