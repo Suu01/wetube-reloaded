ff61982ea7940f4816ac89c3add6a838f05f1391
Suu01 <92988188+Suu01@users.noreply.github.com>
2023-06-07 22:29:26 +0900

    middlewares

--- src/server.js
+++ src/server.js
@@ -1,28 +1,50 @@
 import express from "express";
-// const express = require("express");
 
 const PORT = 4000;
 
 const app = express();
 
-// const handleHome = (req, res) => console.log("Somebody is trying to go home.");
-const handleHome = (req, res) => {
-  //   console.log(req);
-  //   console.log(res);
-  //   return res.end();
-  return res.send("<h1>I still love you.</h1>");
+// const logger = (req, res, next) => {
+//   console.log("I'm in the middle");
+//   // return res.send("lalala");
+//   next();
+// };
+
+const logger = (req, res, next) => {
+  // console.log(`Someone is going to: ${req.url}`);
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+const provateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
+  next();
 };
 
-const handleLogin = (req, res) => {
-  return res.send({ message: "Login here." });
+const handleHome = (req, res) => {
+  // return res.end();
+  return res.send("I love middlewares");
 };
+
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+// app.get("/", logger, handleHome);
+
+// app.get("/", handleHome);
+// app.use(logger);
+
+app.use(logger);
+app.use(provateMiddleware);
 
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
-
-// app.listen(4000, console.log("Server listening on port 4000"));
---
+++ src/server1.js
@@ -0,0 +1,28 @@
+import express from "express";
+// const express = require("express");
+
+const PORT = 4000;
+
+const app = express();
+
+// const handleHome = (req, res) => console.log("Somebody is trying to go home.");
+const handleHome = (req, res) => {
+  //   console.log(req);
+  //   console.log(res);
+  // return res.end();
+  return res.send("<h1>I still love you.</h1>");
+};
+
+const handleLogin = (req, res) => {
+  return res.send({ message: "Login here." });
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
+
+// app.listen(4000, console.log("Server listening on port 4000"));
