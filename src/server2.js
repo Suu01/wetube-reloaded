c5b2bedc9b0ee6d29ba7440716521b8a811c9079
Suu01 <92988188+Suu01@users.noreply.github.com>
2023-06-07 19:57:24 +0900

    challenge answer

--- src/server2.js
+++ src/server2.js
@@ -29,3 +29,15 @@ const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
+
+// nico answer
+// import express from "express";
+
+// const app = express();
+
+// app.get("/", (req, res) => res.send("<h1>Home</h1>"));
+// app.get("/about", (req, res) => res.send("<h1>About</h1>"));
+// app.get("/contact", (req, res) => res.send("<h1>Contact</h1>"));
+// app.get("/login", (req, res) => res.send("<h1>Login</h1>"));
+
+// app.listen(() => `Listening!✅`);
