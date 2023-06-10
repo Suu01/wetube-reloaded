3fd2cff82c560f734d64a7fc4443b7ba281b3b79
Suu01 <92988188+Suu01@users.noreply.github.com>
2023-06-10 17:44:46 +0900

    pub

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,17 +1,20 @@
 export const trending = (req, res) =>
-  res.send(
-    "<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>"
-  );
+  res.render("home", { pageTitle: "Home", potato: "Tomato" });
+// res.send(
+//   "<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>"
+// );
 export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(
-    `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Watch video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
-  );
+  // console.log(req.params);
+  // return res.send(
+  //   `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Watch video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
+  // );
+  return res.render("watch", { pageTitle: "Watch" });
 };
 export const edit = (req, res) => {
-  return res.send(
-    `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Edit video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
-  );
+  // return res.send(
+  //   `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Edit video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
+  // );
+  return res.render("edit", { pageTitle: "Edit" });
 };
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
