5122eedae5fe1827c9b2602182b1c0b313180423
Suu01 <92988188+Suu01@users.noreply.github.com>
2023-06-14 13:33:32 +0900

    array-db

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,61 +1,96 @@
-const fakeUser = {
-  username: "Nicolas",
-  loggedIn: false,
-};
-
-export const trending = (req, res) => {
-  const videos = [
-    // {
-    //   title: "Hello",
-    // },
-    // {
-    //   title: "Video #2",
-    // },
-    // {
-    //   title: "Whatsup",
-    // },
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 2,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 3,
-    },
-  ];
-  // const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  return res.render("home", {
-    pageTitle: "Home",
-    videos,
-    potato: "Tomato",
-    fakeUser: fakeUser,
-  });
-};
+// const fakeUser = {
+//   username: "Nicolas",
+//   loggedIn: false,
+// };
+// export const trending = (req, res) => {
+//   const videos = [
+//     // {
+//     //   title: "Hello",
+//     // },
+//     // {
+//     //   title: "Video #2",
+//     // },
+//     // {
+//     //   title: "Whatsup",
+//     // },
+//     {
+//       title: "First Video",
+//       rating: 5,
+//       comments: 2,
+//       createdAt: "2 minutes ago",
+//       views: 59,
+//       id: 1,
+//     },
+//     {
+//       title: "Second Video",
+//       rating: 5,
+//       comments: 2,
+//       createdAt: "2 minutes ago",
+//       views: 59,
+//       id: 2,
+//     },
+//     {
+//       title: "Third Video",
+//       rating: 5,
+//       comments: 2,
+//       createdAt: "2 minutes ago",
+//       views: 59,
+//       id: 3,
+//     },
+//   ];
+//   // const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//   return res.render("home", {
+//     pageTitle: "Home",
+//     videos,
+//     potato: "Tomato",
+//     fakeUser: fakeUser,
+//   });
+// };
 // res.send(
 //   "<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>"
 // );
+
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => {
   // console.log(req.params);
   // return res.send(
   //   `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Watch video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
   // );
-  return res.render("watch", { pageTitle: "Watch" });
+  const { id } = req.params;
+  // const id = req.params.id;
+  // console.log("Show video", id);
+  const video = videos[id - 1];
+  // return res.render("watch", { pageTitle: "Watch" });
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
 };
 export const edit = (req, res) => {
   // return res.send(
