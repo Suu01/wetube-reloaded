d559d4c95adeb62f53fb34a96505eba62ff5bc4f
Suu01 <92988188+Suu01@users.noreply.github.com>
2023-06-14 22:31:27 +0900

    upload

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -112,11 +112,21 @@ export const postEdit = (req, res) => {
 };
 
 export const getUpload = (req, res) => {
-  return res.render("upload");
+  return res.render("upload", { pageTitle: "Upload Video" });
 };
 
 export const postUpload = (req, res) => {
-  // here we will add a video to the videos array.
+  const { title } = req.body;
+  const newVideo = {
+    title: title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  // console.log(req.body);
   return res.redirect("/");
 };
 
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,6 +3,8 @@ import {
   watch,
   getEdit,
   postEdit,
+  getUpload,
+  postUpload,
   upload,
   deleteVideo,
 } from "../controllers/videoControllers";
@@ -13,7 +15,10 @@ videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 // videoRouter.get("/:id(\\d+)/edit", getEdit);
 // videoRouter.post("/:id(\\d+)/edit", postEdit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-videoRouter.get("/upload", upload);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
+// videoRouter.get("/upload", getUpload);
+// videoRouter.post("/upload", postUpload);
+// videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+// videoRouter.get("/upload", upload);
 
 export default videoRouter;
