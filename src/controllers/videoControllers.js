ab12ed39631c0605cb3f2076cb4a5a7e85dba341
Suu01 <92988188+Suu01@users.noreply.github.com>
2023-06-20 15:59:08 +0900

    upload

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -153,7 +153,19 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = (req, res) => {
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  // console.log(title, description, hashtags);
+  const video = new Video({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  console.log(video);
   // const newVideo = {
   //   title: title,
   //   rating: 0,
