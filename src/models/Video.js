daa8b88cff29d32cd562b5722464b463df13c005
Suu01 <92988188+Suu01@users.noreply.github.com>
2023-06-21 12:15:36 +0900

    edit

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
+// export const formatHashtags = (hashtags) =>
+//   hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
+
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 80 },
   description: { type: String, required: true, trim: true, minLength: 20 },
@@ -11,13 +14,19 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
-videoSchema.pre("save", async function () {
-  // console.log(this);
-  // this.title = "Hahaha! I'm a middleware";
-  this.hashtags = this.hashtags[0]
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
     .split(",")
     .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
+
+// videoSchema.pre("save", async function () {
+//   // console.log(this);
+//   // this.title = "Hahaha! I'm a middleware";
+//   this.hashtags = this.hashtags[0]
+//     .split(",")
+//     .map((word) => (word.startsWith("#") ? word : `#${word}`));
+// });
 
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
